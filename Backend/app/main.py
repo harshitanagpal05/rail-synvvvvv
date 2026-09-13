8015701c34e94fdb9fc8d5beb149703bbb9f54ef
Harshita Nagpal <harshita.nagpal005@gmail.com>
2026-09-13 13:16:25 +0530

    fix(cors): allow origin regex for seamless production deployment

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -54,6 +54,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.cors_origin_list,
+    allow_origin_regex=r"https?://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
